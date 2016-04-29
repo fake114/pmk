de0eaa33c34fda5875fd9674ceb41ad90ec01e2a
Derek <derek.lee@web.de>
2016-04-30 01:31:00 +0200

    Habe lustige und primitive GUI angefangen

--- app/src/main/java/com/example/black/pmk/MainActivity.java
+++ app/src/main/java/com/example/black/pmk/MainActivity.java
@@ -2,10 +2,18 @@ package com.example.black.pmk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.black.pmk.threading.HL7Worker;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button temperatureButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         HL7Worker worker = new HL7Worker();
         worker.execute();
+        temperatureButton = (Button) findViewById(R.id.temperatureButton);
+    }
+
+
+    public void updateTemperatureButton(View v){
+        Random random = new Random();
+        Integer i = random.nextInt(5);
+        i += 35;
+        temperatureButton.setText(i.toString() + " C°");
+
 
     }
+
 }
